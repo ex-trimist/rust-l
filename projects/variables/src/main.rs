417c5c730af6692c19aa7dc9d5a2023105a9da03
ex-trimist <thextremist09@gmail.com>
2022-07-29 00:59:31 +0530

    some updates

---
+++ projects/variables/src/main.rs
@@ -0,0 +1,27 @@
+fn main() {
+    // let mut x = 5;
+    // println!("{x}");
+    // x = 6;
+    // println!("{x}");
+
+    // let x = 5;
+
+    // let x = x + 1;
+
+    // {
+    //     let x = x * 2;
+    //     println!("The value of x in the inner scope is: {x}");
+    // }
+
+    // println!("The value of x is: {x}");
+
+    // let guess: i64 = "42".parse().expect("Not a number!");
+    // let t = true;
+
+    // let f: bool = false; // with explicit type annotation
+    let c = 'z';
+    let z: char = 'ℤ'; // with explicit type annotation
+    let heart_eyed_cat: char = '😻';
+
+    println!("{c} {z} {heart_eyed_cat}");
+}
